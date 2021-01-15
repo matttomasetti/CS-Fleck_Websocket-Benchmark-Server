00da55e9ac5d3a83cfdf0fae5083c260894ae360
Matt Tomasetti <matt@bizmono.com>
2021-01-15 00:06:43 -0500

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu
+
+# Set timezone
+ENV TZ=America/New_York
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Add source files to docker image
+ADD .	/root
+
+# Update and install dependencies
+RUN apt-get -y update \
+    && apt-get -y upgrade \
+    && apt-get -y install wget \
+    && wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb \
+    && dpkg -i packages-microsoft-prod.deb \
+    && apt-get update \
+    && apt-get install -y apt-transport-https \
+    && apt-get update \
+    && apt-get install -y dotnet-sdk-5.0
+    
+## compile websocket
+RUN cd /root \
+    && dotnet build
+
+    
+EXPOSE 8080
+
+WORKDIR /root
+CMD ["dotnet", "run", "--project", "cs_websocket_benchmark_server", "--framework", "netcoreapp5.0"]
---
+++ cs_fleck_websocket_benchmark_server/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Fleck;
+using Newtonsoft.Json.Linq;
+
+namespace fleck_socket
+{
+    class Program
+    {
+        /// <summary>
+        /// Creates the websocket server object with the necessary event handlers, and
+        /// starts listening on the given port
+        /// </summary>
+        /// <param name="args"> </param>
+        static void Main(string[] args)
+        {
+            // Create a listen server on localhost with port 8080
+            var server = new WebSocketServer("ws://0.0.0.0:8080");
+            server.Start(socket =>
+            {
+                
+                /*
+                 * Bind required events for the server
+                 */
+            
+                // Event triggered whenever a client connects to the websocket
+                socket.OnOpen = () =>
+                {
+                    // send newly connected client initial timestamp
+                    Notify(socket, 0);
+                };
+                
+                // Event triggered whenever a client disconnects to the websocket
+                socket.OnClose = () => Console.WriteLine("Close!");
+                
+                // Event triggered whenever the websocket server receives a message form a client
+                socket.OnMessage = message =>
+                {
+                    // decode incoming message into an object
+                    JObject json = JObject.Parse(message);
+                    var c = json.GetValue("c").ToString();
+                
+                    // notify client with event for message with count "c"
+                    Notify(socket, int.Parse(c));
+                };
+            });
+
+            Console.ReadLine();
+        }
+        
+        /// <summary>
+        /// Gets the current unix timestamp of the server
+        /// </summary>
+        /// <returns> The current unix timestamp </returns>
+        private static long GetTimestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+        
+        /// <summary>
+        /// Creates a JSON string containing the message count and the current timestamp
+        /// </summary>
+        /// <param name="c"> The message count </param>
+        /// <returns>  A JSON string containing the message count and the current timestamp </returns>
+        private static string GetEvent(int c)
+        {
+            //create an event array for the time that message "c" is received by the server
+            return String.Format("{{\"c\":{0},\"ts\":{1}}}", c.ToString(), GetTimestamp().ToString());
+        }
+
+        /// <summary>
+        /// Send a connected client an event JSON string
+        /// </summary>
+        /// <param name="ws"> The client connection the outgoing message is for </param>
+        /// <param name="c"> The message count </param>
+        private static void Notify(IWebSocketConnection ws, int c)
+        {
+            //send the given connection the event timestamp for message "c"
+            ws.Send(GetEvent(c));
+        }
+    }
+}
